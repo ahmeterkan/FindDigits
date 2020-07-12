ca43621bfc6a8ce309a68e8ce99a8763424fbc42
ahmeterkan <ahmete48@hotmail.com>
2020-07-13 00:18:36 +0300

    13/07/2020

---
+++ FindDigits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FindDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1012;
+            
+            int count = 0, digits = 0;
+            int temp = n;
+            do
+            {
+                digits = n % 10;
+                n = n / 10;
+                if (digits != 0 && temp % digits == 0)
+                {
+                    count++;
+                }
+            } while (n != 0);
+            Console.WriteLine(count);
+        }
+    }
+}
